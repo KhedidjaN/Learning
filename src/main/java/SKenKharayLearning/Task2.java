061f8f2443b0390b554dd8883648c024450869c4
sara leila <slaynemri@gmail.com>
2022-12-24 07:11:02 -0500

    first class slay 2022

---
+++ src/main/java/SKenKharayLearning/Task2.java
@@ -0,0 +1,16 @@
+package SKenKharayLearning;
+
+public class Task2 {
+    public static void main(String[] args) {
+        /*
+        Print numbers from 1 to 50 except those that are divisible by 3
+         */
+
+        for (int i = 1; i <= 50; i++) {
+            if (i % 3 == 0) {
+                continue;
+            }
+            System.out.println(i + " ");
+        }
+    }
+}
